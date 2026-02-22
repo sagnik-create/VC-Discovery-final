ac9addc9e2e90f74b58edabd831e27ffa9072c3d
Sagnik Dey <sagnikd587@gmail.com>
2026-02-22 23:23:11 +0530

    fixed cors

--- backend/db.py
+++ backend/db.py
@@ -1,6 +1,13 @@
 from sqlalchemy import create_engine, Column, String, DateTime, JSON
 from sqlalchemy.orm import declarative_base, sessionmaker
 from datetime import datetime
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
